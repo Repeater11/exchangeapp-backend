4a95eb21e330faee714f075c32b9df677550b8db
Repeater <532787738@qq.com>
2025-12-18 23:51:37 +0800

    feat(db): 添加 MySQL 连接初始化

---
+++ internal/db/mysql.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"exchangeapp/internal/config"
+	"fmt"
+	"net/url"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func NewMySQL(cfg *config.DatabaseConfig) (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%d)/%s?parseTime=true&charset=utf8mb4&loc=Local",
+		cfg.User,
+		url.QueryEscape(cfg.Password),
+		cfg.Host,
+		cfg.Port,
+		cfg.Name,
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("创建数据库失败：%w", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, fmt.Errorf("获取底层连接失败：%w", err)
+	}
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(50)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	return db, nil
+}
