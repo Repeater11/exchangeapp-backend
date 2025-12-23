f204d29e879d2e03d27bc5403d55d984ca5abc40
Repeater <532787738@qq.com>
2025-12-23 22:50:56 +0800

    feat(server): 初始化 Gin 服务并增加健康检查

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"exchangeapp/internal/app"
 	"exchangeapp/internal/config"
-	"fmt"
 	"log"
 )
 
@@ -11,5 +11,13 @@ func main() {
 	if err != nil {
 		log.Fatalf("配置加载失败：%v", err)
 	}
-	fmt.Println(cfg.Database.Port)
+
+	s, err := app.NewServer()
+	if err != nil {
+		log.Fatalf("新建服务失败：%v", err)
+	}
+
+	if err = s.Run(cfg.App.Port); err != nil {
+		log.Fatalf("服务启动失败：%v", err)
+	}
 }
--- internal/app/server.go
+++ internal/app/server.go
@@ -1,6 +1,11 @@
 package app
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type Server struct {
 	e *gin.Engine
@@ -10,5 +15,15 @@ func NewServer() (*Server, error) {
 	e := gin.New()
 	e.Use(gin.Logger(), gin.Recovery())
 
+	e.GET("/healthz", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
 	return &Server{e: e}, nil
+}
+
+func (s *Server) Run(addr int) error {
+	return s.e.Run(fmt.Sprintf(":%d", addr))
 }
